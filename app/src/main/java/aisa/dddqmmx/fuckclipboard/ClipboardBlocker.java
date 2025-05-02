7fde3e3b38a6fb8513021d3776e04fa4c8c5b7b2
PaulPerkenstein <58079930+dddqmmx@users.noreply.github.com>
2025-05-02 10:51:05 +0800

    白名单测试

--- app/src/main/java/aisa/dddqmmx/fuckclipboard/ClipboardBlocker.java
+++ app/src/main/java/aisa/dddqmmx/fuckclipboard/ClipboardBlocker.java
@@ -1,6 +1,8 @@
 package aisa.dddqmmx.fuckclipboard;
 
-import java.lang.reflect.Method;
+import android.app.AppOpsManager;
+
+import java.lang.reflect.Field;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -10,32 +12,54 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class ClipboardBlocker implements IXposedHookLoadPackage {
 
+    private static int opWriteClipboard;
+
+    static {
+        try {
+            // 缓存反射字段
+            Field opWriteClipboardField = XposedHelpers.findField(AppOpsManager.class, "OP_WRITE_CLIPBOARD");
+            opWriteClipboard = (int) opWriteClipboardField.get(null);
+        } catch (Exception e) {
+            XposedBridge.log("Error initializing AppOpsManager field: " + e);
+        }
+    }
+
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!"android".equals(lpparam.packageName)) {
             return;
         }
+
         try {
             Class<?> clipboardService = XposedHelpers.findClass("com.android.server.clipboard.ClipboardService", lpparam.classLoader);
-            try {
-                XposedHelpers.findAndHookMethod(
-                        clipboardService,
-                        "getClipboardLocked",
-                        int.class,
-                        int.class,
-                        new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                param.setResult(null);
-                            }
-                        });
-            } catch (Exception e) {
-                XposedBridge.log("setPrimaryClipInternalLocked not found: " + e);
-            }
+
+            hookClipboardAccessMethod(clipboardService);
 
             XposedBridge.log("ClipboardBlocker hooked successfully");
         } catch (Throwable t) {
             XposedBridge.log("Hook failed: " + t + " 包名: " + lpparam.packageName + " 进程名: " + lpparam.processName);
+        }
+    }
+
+    private void hookClipboardAccessMethod(Class<?> clipboardService) {
+        try {
+            XposedHelpers.findAndHookMethod(clipboardService, "clipboardAccessAllowed", int.class, String.class, String.class, int.class, int.class, int.class, boolean.class, boolean.class, boolean.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            int op = (int) param.args[0]; // 第一个参数
+                            if (op == opWriteClipboard) {
+                                return;
+                            }
+                            String callingPackage = (String) param.args[1]; // 第二个参数
+                            if ("com.google.android.inputmethod.latin".equals(callingPackage)) {
+                                return;
+                            }
+                            param.setResult(false); // 拒绝访问剪贴板
+                        }
+                    });
+        } catch (Exception e) {
+            XposedBridge.log("Error hooking clipboardAccessAllowed: " + e);
         }
     }
 }
